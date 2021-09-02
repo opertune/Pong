5b090afc40e8e833a9f1bae1e6bb1b56e7060e39
opertune <r.baucheron@gmail.com>
2021-09-02 16:31:24 +0200

    reset bug fix

--- src/main/java/com/example/pong/Palet.java
+++ src/main/java/com/example/pong/Palet.java
@@ -3,6 +3,7 @@ package com.example.pong;
 import javafx.animation.AnimationTimer;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
@@ -33,7 +34,7 @@ public class Palet {
     private static double angle = Math.atan2(INITIAL_VY, INITIAL_VX);
     private static double magnitude = Math.sqrt(3);
 
-    private static double deltaX  = magnitude * Math.cos(angle);
+    private static double deltaX = magnitude * Math.cos(angle);
     private static double deltaY = magnitude * Math.sin(angle);
 
     public void moveP(){
@@ -42,38 +43,31 @@ public class Palet {
             public void handle(long l) {
                 // Check collision with player 1
                 if(_rctJoueur1.getBoundsInParent().intersects(_crcPalet.getBoundsInParent())){
-                    magnitude *= (magnitude < SPEED_LIMITE) ? ACCELERATION : 1;
-                    angle = (Math.PI / 4) * Math.abs((_rctJoueur1.getY() + 75 - _crcPalet.getCenterY() - 12) / 75);
-                    deltaX = Math.abs(magnitude * Math.cos(angle));
-                    deltaY = (_crcPalet.getCenterY() <= _rctJoueur1.getY() + 75 ? -Math.abs(magnitude * Math.sin(angle)) : Math.abs(magnitude * Math.sin(angle)));
+                    collisionPlayer(true);
+
                     // Check collision with player 2
                 }else if(_rctJoueur2.getBoundsInParent().intersects(_crcPalet.getBoundsInParent())){
-                    magnitude *= (magnitude < SPEED_LIMITE) ? ACCELERATION : 1;
-                    deltaX = -deltaX;
+                    collisionPlayer(false);
+
                     // Check collision with bottom frame
                 }else if(_crcPalet.getCenterY() > BOTTOM_LIMIT){
                     deltaY = -deltaY;
+
                     // Check collision with top frame
                 }else if(_crcPalet.getCenterY() < 0){
                     deltaY = -deltaY;
                 }
 
-                // if palet is out of box : Lose and turn inGame bool to false
+                // if palet is out of box Left side
                 if(_crcPalet.getCenterX() < -15){
-                    _lblWinLose.setText("Player 1 Lose !");
-                    _lblWinLose.setTextFill(Color.RED);
-                    // Increase player score
-                    MainController.p2Score++;
-                    _lblScore.setText("Score : " + MainController.p1Score + " - " + MainController.p2Score);
-                    inGame = false;
+                    outOfGrid("Player 1 Lose !", true);
+                    // Stop AnimationTimer loop
                     stop();
+
+                    // if palet is out of box Right side
                 }else if(_crcPalet.getCenterX() > 615){
-                    _lblWinLose.setText("Player 2 Lose !");
-                    _lblWinLose.setTextFill(Color.RED);
-                    // Increase player score
-                    MainController.p1Score++;
-                    _lblScore.setText("Score : " + MainController.p1Score + " - " + MainController.p2Score);
-                    inGame = false;
+                    outOfGrid("Player 2 Lose !", false);
+                    // Stop AnimationTimer loop
                     stop();
                 }
 
@@ -83,5 +77,42 @@ public class Palet {
             }
         };
         timer.start();
+    }
+
+    // Collision between player and palet
+    void collisionPlayer(boolean player1){
+        // increased palet speed
+        magnitude *= (magnitude < SPEED_LIMITE) ? ACCELERATION : 1;
+
+        // more the palet are close to the center less the angle are wide
+        angle = (Math.PI / 4) * Math.abs((_rctJoueur1.getY() + 75 - _crcPalet.getCenterY() - 12) / 75);
+        if (player1){
+            deltaX = Math.abs(magnitude * Math.cos(angle));
+        }else{
+            deltaX = -Math.abs(magnitude * Math.cos(angle));
+        }
+
+        // value depends on the place or hits the palet : high or low half
+        deltaY = (_crcPalet.getCenterY() <= _rctJoueur1.getY() + 75 ? -Math.abs(magnitude * Math.sin(angle)) : Math.abs(magnitude * Math.sin(angle)));
+    }
+
+    // Palet out of the grid (left and right side)
+    void outOfGrid(String txt, boolean player1){
+        _lblWinLose.setText(txt);
+        _lblWinLose.setTextFill(Color.RED);
+
+        // Increase player score
+        if(player1){
+            MainController.p2Score++;
+        }else{
+            MainController.p1Score++;
+        }
+        _lblScore.setText("Score : " + MainController.p1Score + " - " + MainController.p2Score);
+        inGame = false;
+
+        // Reset magnitude and delta
+        magnitude = Math.sqrt(3);
+        deltaX = magnitude * Math.cos(Math.atan2(0, 0));
+        deltaY = magnitude * Math.sin(Math.atan2(0, 0));
     }
 }
